b63fb08be0f9f6d20a9be0f7142138d9add51b61
Rick Bruch <rbruch@u.washington.edu>
2013-09-05 11:21:22 -0700

    Copied vocabulary functions into Select nested widget.py

--- eea/facetednavigation/widgets/select-nested/widget.py
+++ eea/facetednavigation/widgets/select-nested/widget.py
@@ -1,5 +1,7 @@
-""" Select widget
+""" Select nested widget
 """
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import safeToInt
 from Products.Archetypes.public import Schema
 from Products.Archetypes.public import BooleanField
 from Products.Archetypes.public import StringField
@@ -71,6 +73,81 @@ class Widget(CountableWidget):
 
     index = ViewPageTemplateFile('widget.pt')
     edit_schema = CountableWidget.edit_schema.copy() + EditSchema
+
+    def catalog_vocabulary(self):
+        """ Get vocabulary from catalog
+        """
+        catalog = self.data.get('catalog', 'portal_catalog')
+        ctool = getToolByName(self.context, catalog)
+        if not ctool:
+            return []
+
+        index = self.data.get('index', None)
+        if not index:
+            return []
+
+        index = ctool.Indexes.get(index, None)
+        if not index:
+            return []
+
+        res = []
+        for val in index.uniqueValues():
+            if isinstance(val, (int, float)):
+                val = unicode(str(val), 'utf-8')
+
+            elif not isinstance(val, unicode):
+                try:
+                    val = unicode(val, 'utf-8')
+                except Exception:
+                    continue
+
+            val = val.strip()
+            if not val:
+                continue
+
+            res.append(val)
+
+        return res
+
+    def portal_vocabulary(self):
+        """Look up selected vocabulary from portal_vocabulary or from ZTK
+           zope-vocabulary factory.
+        """
+        vtool = getToolByName(self.context, 'portal_vocabularies', None)
+        voc_id = self.data.get('vocabulary', None)
+        if not voc_id:
+            return []
+        voc = getattr(vtool, voc_id, None)
+        if not voc:
+            voc = queryUtility(IVocabularyFactory, voc_id, None)
+            if voc:
+                return [(term.value, (term.title or term.token or term.value))
+                        for term in voc(self.context)]
+            return []
+
+        terms = voc.getDisplayList(self.context)
+        if hasattr(terms, 'items'):
+            return terms.items()
+        return terms
+
+    def vocabulary(self, **kwargs):
+        """ Return data vocabulary
+        """
+        reverse = safeToInt(self.data.get('sortreversed', 0))
+        mapping = self.portal_vocabulary()
+        catalog = self.data.get('catalog', None)
+
+        if catalog:
+            mapping = dict(mapping)
+            values = self.catalog_vocabulary()
+            res = [(val, mapping.get(val, val)) for val in values]
+            res.sort(key=operator.itemgetter(1), cmp=compare)
+        else:
+            res = mapping
+
+        if reverse:
+            res.reverse()
+        return res
 
     def query(self, form):
         """ Get value from form and return a catalog dict query
